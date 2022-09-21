a841a9dcb1c7e342e75da43b98aedaf69232cd20
chukwuma <cmaduakonam@yahoo.com>
2022-09-21 11:52:06 +0100

    added navlinks.js
    The files was added to the main folder of Navbar

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Navbar
+
 Create Navbar using React Js and React Router
+
+# Sub header
+
+Watch the git tutorial on youtube
---
+++ navlink.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function navlink() {
+  return (
+    <div>
+        <pa>Home</pa>
+        <pa>Contact Us</pa>
+        <pa>Services</pa>
+        <pa>Register</pa>
+    </div>
+  )
+}
